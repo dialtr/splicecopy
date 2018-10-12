4b2857e98b8b81e9e5e2311020241941ea3aca89
Thomas R. Dial <dialtr@google.com>
2018-10-12 00:24:28 -0400

    working

--- main.cc
+++ main.cc
@@ -5,8 +5,8 @@
 #include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/epoll.h>
 #include <string.h>
+#include <sys/epoll.h>
 #include <unistd.h>
 
 int usage() {
@@ -14,7 +14,6 @@ int usage() {
          "Usage: fastcopy <src> <dest>\n");
   return 0;
 }
-
 
 int main(int argc, char *argv[]) {
   // We use the reentrant version of strerror, strerror_r, so we need this.
@@ -35,13 +34,13 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-	// Get the size of the input (src) file.
-	const size_t src_file_size = src_file->GetFileSize();
-	if (src_file_size < 0) {
-		fprintf(stderr, "%s: failed to get size of input file.\n",
-				strerror_r(errno, errbuf, sizeof(errbuf)));
-		return 1;
-	}
+  // Get the size of the input (src) file.
+  const size_t src_file_size = src_file->GetFileSize();
+  if (src_file_size < 0) {
+    fprintf(stderr, "%s: failed to get size of input file.\n",
+            strerror_r(errno, errbuf, sizeof(errbuf)));
+    return 1;
+  }
 
   // Make the input file nonblocking.
   if (!src_file->MakeNonblocking()) {
@@ -51,10 +50,10 @@ int main(int argc, char *argv[]) {
   }
 
   // Open the output file.
-	const int flag_bits = O_CREAT | O_WRONLY;
-	const int mode_bits = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-  auto dest_file = std::make_shared<FileDescriptor>(
-			open(argv[2], flag_bits, mode_bits));
+  const int flag_bits = O_CREAT | O_WRONLY;
+  const int mode_bits = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+  auto dest_file =
+      std::make_shared<FileDescriptor>(open(argv[2], flag_bits, mode_bits));
   if (dest_file->Get() < 0) {
     fprintf(stderr, "%s: opening '%s' for writing.\n",
             strerror_r(errno, errbuf, sizeof(errbuf)), argv[2]);
@@ -91,100 +90,100 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-	// Create an epoll object to manage event-driven I/O.
+  // Create an epoll object to manage event-driven I/O.
   auto epoller = std::make_shared<FileDescriptor>(epoll_create1(0));
-	if (epoller->Get() < 0) {
-		fprintf(stderr, "%s: failed to create epoll object.\n",
-				strerror_r(errno, errbuf, sizeof(errbuf)));
-		return 1;
-	}
+  if (epoller->Get() < 0) {
+    fprintf(stderr, "%s: failed to create epoll object.\n",
+            strerror_r(errno, errbuf, sizeof(errbuf)));
+    return 1;
+  }
 
-	// Aliases to avoid repeated calls to FileDescriptor::Get() in the loop.
-	const int EPOLL_FD = epoller->Get();
-	const int SOURCE_FILE = src_file->Get();
-	const int DEST_FILE = dest_file->Get();
-	const int PIPE_READER = read_pipe->Get();
-	const int PIPE_WRITER = write_pipe->Get();
+  // Aliases to avoid repeated calls to FileDescriptor::Get() in the loop.
+  const int EPOLL_FD = epoller->Get();
+  const int SOURCE_FILE = src_file->Get();
+  const int DEST_FILE = dest_file->Get();
+  const int PIPE_READER = read_pipe->Get();
+  const int PIPE_WRITER = write_pipe->Get();
 
-	// Add the write side of the pipe to the poll set.
-	epoll_event ev;
-	ev.events = EPOLLOUT;
-	ev.data.fd = PIPE_WRITER;
-	status = epoll_ctl(epoller->Get(), EPOLL_CTL_ADD, write_pipe->Get(), &ev);
-	if (status == -1) {
-		fprintf(stderr, "%s: failed to add write side of pipe to epoll set.\n",
-				strerror_r(errno, errbuf, sizeof(errbuf)));
-		return 1;
-	}
+  // Add the write side of the pipe to the poll set.
+  epoll_event ev;
+  ev.events = EPOLLOUT;
+  ev.data.fd = PIPE_WRITER;
+  status = epoll_ctl(epoller->Get(), EPOLL_CTL_ADD, write_pipe->Get(), &ev);
+  if (status == -1) {
+    fprintf(stderr, "%s: failed to add write side of pipe to epoll set.\n",
+            strerror_r(errno, errbuf, sizeof(errbuf)));
+    return 1;
+  }
 
-	// Add the read side of the pipe to the poll set.
-	ev.events = EPOLLIN;
-	ev.data.fd = PIPE_READER;
-	status = epoll_ctl(epoller->Get(), EPOLL_CTL_ADD, read_pipe->Get(), &ev);
-	if (status == -1) {
-		fprintf(stderr, "%s: failed to add write side of pipe to epoll set.\n",
-				strerror_r(errno, errbuf, sizeof(errbuf)));
-		return 1;
-	}
+  // Add the read side of the pipe to the poll set.
+  ev.events = EPOLLIN;
+  ev.data.fd = PIPE_READER;
+  status = epoll_ctl(epoller->Get(), EPOLL_CTL_ADD, read_pipe->Get(), &ev);
+  if (status == -1) {
+    fprintf(stderr, "%s: failed to add write side of pipe to epoll set.\n",
+            strerror_r(errno, errbuf, sizeof(errbuf)));
+    return 1;
+  }
 
-	// We'll dequeue at most 2 events each pass, since there are two fd's.
-	const int NUM_EVENTS = 2;
-	struct epoll_event events[NUM_EVENTS] = {0};
+  // We'll dequeue at most 2 events each pass, since there are two fd's.
+  const int NUM_EVENTS = 2;
+  struct epoll_event events[NUM_EVENTS] = {0};
 
-	// Track the number of bytes written to the output file (the copy).
-	size_t bytes_written_to_file = 0;
+  // Track the number of bytes written to the output file (the copy).
+  size_t bytes_written_to_file = 0;
 
-	// Loop, transferring data using splice operations until we
-	// have transffered all of the data to the destination.
-	while (bytes_written_to_file < src_file_size) {
-		// Wait for readability, writeability on the ends of the pipe.
-		const int nfds = epoll_wait(EPOLL_FD, events, NUM_EVENTS, -1);
-		if (nfds == -1) {
-			fprintf(stderr, "%s: epoll_wait() failed.\n",
-					strerror_r(errno, errbuf, sizeof(errbuf)));
-			return 1;
-		}
+  // Loop, transferring data using splice operations until we
+  // have transffered all of the data to the destination.
+  while (bytes_written_to_file < src_file_size) {
+    // Wait for readability, writeability on the ends of the pipe.
+    const int nfds = epoll_wait(EPOLL_FD, events, NUM_EVENTS, -1);
+    if (nfds == -1) {
+      fprintf(stderr, "%s: epoll_wait() failed.\n",
+              strerror_r(errno, errbuf, sizeof(errbuf)));
+      return 1;
+    }
 
-		// Loop on returned events.
-		for (int n = 0; n < nfds; ++n) {
-			if (events[n].data.fd == PIPE_WRITER) {
-				printf("read from input file.\n");
-				// The write side of the pipe is ready for data.
-				const ssize_t xferred = splice(SOURCE_FILE, nullptr,
-						PIPE_WRITER, nullptr, ONE_MEG, SPLICE_F_MOVE | SPLICE_F_NONBLOCK);
+    // Loop on returned events.
+    for (int n = 0; n < nfds; ++n) {
+      if (events[n].data.fd == PIPE_WRITER) {
+        // The write side of the pipe is ready for data.
+        const ssize_t xferred =
+            splice(SOURCE_FILE, nullptr, PIPE_WRITER, nullptr, ONE_MEG,
+                   SPLICE_F_MOVE | SPLICE_F_NONBLOCK);
 
-				// Check for unrecoverable error.
-				if ((xferred == -1) && (errno != EAGAIN)) {
-  			  fprintf(stderr, "%s: splice() failed.\n",
-							strerror_r(errno, errbuf, sizeof(errbuf)));
-		  	  return 1;
-				}
-			} else if (events[n].data.fd == PIPE_READER) {
-				printf("write to output file.\n");
-				// The read size of the pipe is ready for data.
-				const ssize_t xferred = splice(PIPE_READER, nullptr,
-						DEST_FILE, nullptr, ONE_MEG, SPLICE_F_MOVE | SPLICE_F_NONBLOCK);
+        // Check for unrecoverable error.
+        if ((xferred == -1) && (errno != EAGAIN)) {
+          fprintf(stderr, "%s: splice() failed.\n",
+                  strerror_r(errno, errbuf, sizeof(errbuf)));
+          return 1;
+        }
+      } else if (events[n].data.fd == PIPE_READER) {
+        // The read size of the pipe is ready for data.
+        const ssize_t xferred =
+            splice(PIPE_READER, nullptr, DEST_FILE, nullptr, ONE_MEG,
+                   SPLICE_F_MOVE | SPLICE_F_NONBLOCK);
 
-				// Check for unrecoverable error.
-				if (xferred == -1) {
-					if (errno != EAGAIN) {
-  					fprintf(stderr, "%s: splice() failed.\n",
-								strerror_r(errno, errbuf, sizeof(errbuf)));
-						return 1;
-					}
-				} else {
-					// Record that we transferred data to the output file.
-					bytes_written_to_file += xferred;
-				}
+        // Check for unrecoverable error.
+        if (xferred == -1) {
+          if (errno != EAGAIN) {
+            fprintf(stderr, "%s: splice() failed.\n",
+                    strerror_r(errno, errbuf, sizeof(errbuf)));
+            return 1;
+          }
+        } else {
+          // Record that we transferred data to the output file.
+          bytes_written_to_file += xferred;
+        }
 
-			} else {
-				// Totally unexpected error, most likely impossible.
-				fprintf(stderr, "unexpected file descriptor returned from epoll: %d\n",
-						events[n].data.fd);
-				return 1;
-			}
-		}
-	}
-	
+      } else {
+        // Totally unexpected error, most likely impossible.
+        fprintf(stderr, "unexpected file descriptor returned from epoll: %d\n",
+                events[n].data.fd);
+        return 1;
+      }
+    }
+  }
+
   return 0;
 }
